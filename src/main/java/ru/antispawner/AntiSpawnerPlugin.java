8ce6aa9ab87a31f6468a3b8bd50f5e0b028c4405
torpedv <ico.viser@gmail.com>
2026-02-19 18:01:21 +0300

    Create AntiSpawnerPlugin.java

---
+++ src/main/java/ru/antispawner/AntiSpawnerPlugin.java
@@ -0,0 +1,41 @@
+package ru.antispawner;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import java.util.logging.Level;
+
+public class AntiSpawnerPlugin extends JavaPlugin {
+    private static AntiSpawnerPlugin instance;
+    private ConfigManager configManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        configManager = new ConfigManager(this);
+        configManager.loadConfig();
+
+        // Регистрация слушателя
+        getServer().getPluginManager().registerEvents(new SpawnerBreakListener(this), this);
+
+        // Регистрация команды
+        getCommand("asreload").setExecutor(new ReloadCommand(this));
+
+        getLogger().info("AntiSpawner включён. Версия " + getDescription().getVersion());
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("AntiSpawner выключен.");
+    }
+
+    public static AntiSpawnerPlugin getInstance() {
+        return instance;
+    }
+
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+
+    public void reloadPluginConfig() {
+        configManager.reloadConfig();
+    }
+}
